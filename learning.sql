8f28c4970c1fd3d77276e4f621527c00610cfc63
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-22 22:33:09 +0600

    data insertion ,, data print

--- learning.sql
+++ learning.sql
@@ -5,8 +5,22 @@ use college;
 -- creating table 
 CREATE TABLE student(
 -- name datatype constrain 
+-- primary key => id can't be same 
 id INT PRIMARY KEY , 
 name varchar(50) , 
 -- not null = field can't be empty 
 age int not null
-) 
+) ;
+
+-- data insert => insert into table values("value" , "value")
+insert into student values(1, 'monir' , 221);
+insert into student values(2, 'noob monir' , 2210);
+
+
+-- printing data from table 
+select * from student;
+
+
+
+
+
